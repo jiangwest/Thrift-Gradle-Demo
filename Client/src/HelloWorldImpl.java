f8472222b38dd25bb5866b81720878d5a149a229
jiangwest <jiangdm@aliyun.com>
2016-03-28 11:07:37 +0800

    no pain, no gain
    do it now

---
+++ Client/src/HelloWorldImpl.java
@@ -0,0 +1,14 @@
+import org.apache.thrift.TException;
+ 
+
+public class HelloWorldImpl implements HelloWorldService.Iface {
+ 
+	public HelloWorldImpl() {
+	}
+ 
+	@Override
+	public String sayHello(String username) throws TException {
+		return "Hi," + username + " welcome to Wuhan";
+	}
+ 
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Thrift-Gradle-Demo
+
+- aim: 利用Gradle构建Thrift开发环境,无需开发者一个一个下载依赖包
+  - example: [thrift实例以及所需jar包](http://download.csdn.net/detail/hjx_1000/8374829#comment)就预先下载好依赖包，可是并不适合实际情况，开发者难以动态配置
